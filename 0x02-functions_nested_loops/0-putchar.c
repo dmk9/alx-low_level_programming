02ae8b4050239c214366d2d63ab482153a848c87
Dominic Usiabulu <usiabulu.dominic@outlook.com>
2022-09-13 16:21:48 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- *
  * main - entry point
  *
  * Description: print _putchar followed by a newline
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	_putchar('_');

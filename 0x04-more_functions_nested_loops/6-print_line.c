4bddbcd48caf607622d87dfd7eba8c762674fabd
Dominic Usiabulu <usiabulu.dominic@outlook.com>
2022-09-15 18:04:41 -0700

    The shortest distance between two points is a straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,13 +9,17 @@
 
 void print_line(int n)
 {
-	int i = 0;
-
-	if (n > 0)
+	if (n <= 0)
 	{
-		for (; i < n; i++)
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
 			_putchar('_');
+		_putchar('\n');
 	}
 
-	_putchar('\n');
 }

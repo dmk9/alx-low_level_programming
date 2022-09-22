38126bd9cbc66917627909fd89604ba29d6a8335
Dominic Usiabulu <usiabulu.dominic@outlook.com>
2022-09-22 16:59:32 -0700

    print an integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer with _putchar
+ * @n: integer to print
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int i;
+
+	if (n < 0)
+	{
+		i = -n;
+		_putchar('-');
+	}
+	else
+	{
+		i = n;
+	}
+
+	if (i / 10)
+		print_number(i / 10);
+
+	_putchar((i % 10) + '0');
+}

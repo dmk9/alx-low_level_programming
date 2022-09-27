9151085a194f9c6115aeccc35c0147a1e567ac98
Dominic Usiabulu <usiabulu.dominic@outlook.com>
2022-09-26 18:46:10 -0700

    function that searches a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
-/*
- * _strpbrk - function locates the first occurrence in
- * the string s of any of the bytes in the string accept
+/**
+ * _strpbrk - searchesthe string s of any of the bytes in the string accept
  * @s: string to check
  * @accept: string to check against
  *

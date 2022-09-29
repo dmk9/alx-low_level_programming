3e5531220b63b1d0def683491b8a6449ca1defe1
Dominic Usiabulu <usiabulu.dominic@outlook.com>
2022-09-28 18:18:24 -0700

    natural square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number ot take a square root of
+ *
+ * Return: square root of n or -1 if n doesn't have a natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	return (sqrt_checker(1, n));
+}
+
+/**
+ * sqrt_checker - actual checker for square root of k
+ * @j: trials to get square root
+ * @k: number n to get square root of
+ *
+ * Return: natural square root of k or -1
+ */
+int sqrt_checker(int j, int k)
+{
+	if (j * j == k)
+		return (j);
+	if (j * j > k)
+		return (-1);
+}

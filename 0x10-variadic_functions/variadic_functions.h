ca83f8e4a23bf404be91b744900ba7d9c98cf3fa
Dominic Usiabulu <usiabulu.dominic@outlook.com>
2022-10-13 16:51:01 -0700

    function that returns the sum of all its parameters.
    Prototype: int sum_them_all(const unsigned int n, ...);
    If n == 0, return 0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,17 +1,7 @@
 #ifndef _VARIADIC_FUNCTIONS_H_
 #define _VARIADIC_FUNCTIONS_H_
-#include <stdarg.h>
-
-/**
- * struct print - print type with corresponding print function
- * @t: print type
- * @f: print function
- */
-typedef struct print
-{
-	char *t;
-	void (*f)(va_list);
-} print_t;
+#include <stdio.h>
+#include <stdarg.>
 
 int _putchar(char);
 int sum_them_all(const unsigned int n, ...);

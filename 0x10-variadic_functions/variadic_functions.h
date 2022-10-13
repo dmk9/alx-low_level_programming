bd70145daaf90fb0c4421a92c5fd278e9bd8c69a
Dominic Usiabulu <usiabulu.dominic@outlook.com>
2022-10-13 16:55:29 -0700

    function that returns the sum of all its parameters.
    Prototype: int sum_them_all(const unsigned int n, ...);
    If n == 0, return 0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,8 @@
 #ifndef _VARIADIC_FUNCTIONS_H_
 #define _VARIADIC_FUNCTIONS_H_
+
 #include <stdio.h>
-#include <stdarg.>
+#include <stdarg.h>
 
 int _putchar(char);
 int sum_them_all(const unsigned int n, ...);

08ce185adbfc51f0a24b648721bae9e5b836e329
Dominic Usiabulu <usiabulu.dominic@outlook.com>
2022-11-10 05:40:43 -0800

    task 6

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,21 +1,10 @@
 #include <stdio.h>
-
-int main(void);
-extern void _exit(register int);
 /**
- * _start - Executes and print something * before main is executed.
- *
- * Return: 0 (success)
+ * first - function executed before main
+ * Return: no return.
  */
-
-int _start(void)
+void __attribute__ ((constructor)) first()
 {
-	char *str0 = "You're beat! ";
-	char *str1 = "and yet, you must allow,";
-	char *str2 = "\nI bore my house upon my back!\n";
-
-	printf("%s%s%s", str0, str1, str2);
-
-	main();
-	_exit(0);
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
 }

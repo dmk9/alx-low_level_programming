0a0aec5aba091967ce777ffaac180b959acd5eb6
Dominic Usiabulu <usiabulu.dominic@outlook.com>
2022-11-10 02:44:09 -0800

    task 1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,9 +16,9 @@ size_t print_list(const list_t *h)
 
 				printf("[%d] %s\n", 0, "(nil)");
 			else
-				printf("[%d] %s\n", h->len, h->str);
+				printf("[%u] %s\n", h->len, h->str);
 			h = h->next;
-			cont++;
+			cont += 1;
 		}
 		return (cont);
 }

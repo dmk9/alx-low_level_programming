ad03b7889ba5538902d010f53be06957ace5cef8
Dominic Usiabulu <usiabulu.dominic@outlook.com>
2022-11-10 03:36:32 -0800

    task 6

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void);
+extern void _exit(register int);
+/**
+ * _start - Executes and print something * before main is executed.
+ *
+ * Return: 0 (success)
+ */
+
+int _start(void)
+{
+	char *str0 = "You're beat! ";
+	char *str1 = "and yet, you must allow,";
+	char *str2 = "\nI bore my house upon my back!\n";
+
+	printf("%s%s%s", str0, str1, str2);
+
+	main();
+	_exit(0);
+}
